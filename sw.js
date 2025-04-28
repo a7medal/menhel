9a111caa61779208f530dd5a05dcf73065e44516
a7medal <ahmedalahmedbedi@gmail.com>
2025-04-28 06:22:27 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,14 +7,29 @@ const ASSETS = [
   '/script.js',
   '/logo.png',
   '/icon-192192.png',
-  
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // مهم: يجبر تفعيل التحديث الجديد مباشرة
   e.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); // حذف كل الكاشات القديمة
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); // مهم: يسيطر مباشرة على كل الصفحات
 });
 
 self.addEventListener('fetch', (e) => {
